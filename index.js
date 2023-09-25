a0e562ce450b895e9a80f267fe3c90f122722876
MatsalVistry <vatsalmistry4796@gmail.com>
2023-09-25 15:36:09 -0500

    standalone apollo server

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./graphql/schema');
-const { pool } = require('./connection');
-
-const app = express();
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: { pool }
-});
-
 async function startApolloServer() {
-    await server.start();
-  
-    server.applyMiddleware({ app });
-  
-    const PORT = process.env.PORT || 4000;
-  
-    app.listen(PORT, () => {
-      console.log(`Server is running at http://localhost:${PORT}${server.graphqlPath}`);
-    });
+  const { ApolloServer } = require('@apollo/server');
+  const { startStandaloneServer } = require('@apollo/server/standalone');
+  const { typeDefs, resolvers } = require('./graphql/schema');
+  const { pool } = require('./connection');
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: { pool }
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
 }
 
 startApolloServer();
