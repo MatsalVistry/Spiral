cb59bf603dbea367037f8c94ab83b9f1a95989fe
MatsalVistry <vatsalmistry4796@gmail.com>
2023-10-16 01:32:02 -0500

    add last modified to script creation

--- graphql/resolvers/mutations/script.js
+++ graphql/resolvers/mutations/script.js
@@ -2,7 +2,7 @@ const { pool } = require('../../../connection');
 
 const createScript = async (_, { userid, title }) => {
     const client = await pool.connect();
-    const query = 'INSERT INTO scripts(userid, title) VALUES($1, $2, $3) RETURNING *';
+    const query = "INSERT INTO scripts(userid, title, last_modified) VALUES($1, $2, NOW() AT TIME ZONE 'CST6CDT') RETURNING *";
     const values = [userid, title];
     const result = await client.query(query, values);
     client.release();
