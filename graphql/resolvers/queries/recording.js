deb5dc9c4fe89fc7770b2770a1d5dcce4f6770e9
MatsalVistry <vatsalmistry4796@gmail.com>
2023-10-30 10:41:01 -0500

    made time saved into a readable format

--- graphql/resolvers/queries/recording.js
+++ graphql/resolvers/queries/recording.js
@@ -2,7 +2,7 @@ const { pool } = require('../../../connection');
 
 const getScriptRecordings = async(_, { userid, title }) => {
     const client = await pool.connect();
-    const query = "SELECT r.recordingid, s.scriptid, r.time_saved, r.title FROM recordings as r INNER JOIN scripts as s ON s.scriptid=r.scriptid WHERE s.userid = $1 AND s.title = $2";
+    const query = "SELECT r.recordingid, s.scriptid, TO_CHAR(r.time_saved, 'YYYY-MM-DD hh:MI AM') as time_saved, r.title FROM recordings as r INNER JOIN scripts as s ON s.scriptid=r.scriptid WHERE s.userid = $1 AND s.title = $2";
     const values = [userid, title];
     const result = await client.query(query, values);
     client.release();
